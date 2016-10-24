32aba396a943638a3770f27cdf4105c0ea93ac28
Heorhi Sidoryn <heorhi.sidoryn@gmail.com>
2016-10-24 20:39:52 +0300

    fix bootstrap

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 VERSIONS=${VERSIONS:-"2.7.8.10"}
 
-# make directory
-sudo mkdir -p /usr/bin
 cd /usr
 
 sudo wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64.tar.gz
@@ -14,3 +12,5 @@ sudo ln -s /usr/python/bin/easy_install /usr/bin/easy_install
 sudo ln -s /usr/python/bin/pip /usr/bin/pip
 sudo ln -s /usr/python/bin/python /usr/bin/python
 sudo ln -s /usr/python/bin/virtualenv /usr/bin/virtualenv
+
+/usr/bin/pip install docker-py==1.8.1
